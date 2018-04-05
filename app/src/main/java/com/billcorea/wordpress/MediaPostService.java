ab1fca6306f334f47bef08cf815884bb16f73c76
nari4169 <6k2emg@gmail.com>
2018-04-05 23:40:14 +0900

    2018.04.05 Commit ;;;

--- app/src/main/java/com/billcorea/wordpress/MediaPostService.java
+++ app/src/main/java/com/billcorea/wordpress/MediaPostService.java
@@ -6,21 +6,59 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.media.ExifInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Build;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.os.SystemClock;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
+import android.widget.ArrayAdapter;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MediaPostService extends Service {
 
     String TAG = "MediaPostService" ;
+    File file = null ;
 
     private static final int MILLISINFUTURE = 1000*1000;
-    private static final int COUNT_DOWN_INTERVAL = 1000;
+    private static final int COUNT_DOWN_INTERVAL = 30000;
 
     private CountDownTimer countDownTimer;
+    ArrayList<String> fileList = null ;
+
+    Gson gson;
+    List<Object> list;
+    Map<String,Object> mapPost;
+    Map<String,Object> mapId;
+    Map<String,Object> mapTitle;
+    String postTitle[];
+    RequestQueue rQueue ;
+    StringRequest request ;
+    String getMediaSearchUrl = "" ;
 
     public MediaPostService() {
     }
@@ -53,8 +91,9 @@ public class MediaPostService extends Service {
         Notification notification;
 
         notification = new Notification.Builder(getApplicationContext())
-                .setContentTitle("TTT")
-                .setContentText("TTT")
+                .setSmallIcon(R.drawable.ic_notifycation)
+                .setContentTitle("MediaPostService")
+                .setContentText("Catch me if you can !!!")
                 .build();
 
         nm.notify(startId, notification);
@@ -67,7 +106,7 @@ public class MediaPostService extends Service {
     public void onDestroy() {
         super.onDestroy();
 
-        Log.i("PersistentService" , "onDestroy" );
+        Log.i(TAG , "onDestroy" );
         countDownTimer.cancel();
 
         /**
@@ -81,9 +120,34 @@ public class MediaPostService extends Service {
      */
     private void initData(){
 
-
+        fileList = getPathOfAllImages() ;
         countDownTimer();
         countDownTimer.start();
+
+        request = new StringRequest(Request.Method.GET, StringUtil.getUrl(), new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                gson = new Gson();
+                list = (List) gson.fromJson(s, List.class);
+
+                for(int i=0;i<list.size();++i){
+                    mapPost = (Map<String,Object>)list.get(i);
+                    mapId = (Map<String, Object>) mapPost.get("id");
+                    mapTitle = (Map<String, Object>) mapPost.get("guid");
+                    postTitle[i] = (String) mapTitle.get("rendered");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                //Toast.makeText(MainWordPress.this, "Some error occurred", Toast.LENGTH_LONG).show();
+                Log.d(TAG, "Some Error Occurred !!! [" + volleyError.toString() + "]");
+            }
+        });
+
+        request.setRetryPolicy(new DefaultRetryPolicy(10000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES,  DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        rQueue = Volley.newRequestQueue(getBaseContext());
+        rQueue.add(request);
     }
 
     public void countDownTimer(){
@@ -91,22 +155,123 @@ public class MediaPostService extends Service {
         countDownTimer = new CountDownTimer(MILLISINFUTURE, COUNT_DOWN_INTERVAL) {
             public void onTick(long millisUntilFinished) {
 
-                Log.i("PersistentService","onTick");
+                if (getWIFIStatus() && StringUtil.sToken != null) {
+                    Log.i(TAG, "onTick [" + StringUtil.sToken + "]");
+                    //String rootPath = "/storage/572B-3465" ;
+                    //getFileList(rootPath) ;
+
+                    for (String string : fileList)
+                    {
+                        Log.i(TAG, "|" + string + "|");
+                    }
+
+                } else {
+                    Log.d(TAG, "onTick !!! ") ;
+                }
             }
             public void onFinish() {
 
-                Log.i("PersistentService","onFinish");
+                Log.i(TAG,"onFinish");
             }
         };
     }
 
+    private ArrayList<String> getPathOfAllImages()
+    {
+        ArrayList<String> result = new ArrayList<>();
+        Uri uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = { MediaStore.MediaColumns.DATA, MediaStore.MediaColumns.DISPLAY_NAME };
+
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, MediaStore.MediaColumns.DATE_ADDED + " desc");
+        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        int columnDisplayname = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME);
+
+        int lastIndex;
+        while (cursor.moveToNext())
+        {
+            String absolutePathOfImage = cursor.getString(columnIndex);
+            String nameOfFile = cursor.getString(columnDisplayname);
+            lastIndex = absolutePathOfImage.lastIndexOf(nameOfFile);
+            lastIndex = lastIndex >= 0 ? lastIndex : nameOfFile.length() - 1;
+
+            if (!TextUtils.isEmpty(absolutePathOfImage))
+            {
+                result.add(absolutePathOfImage);
+            }
+        }
+
+        for (String string : result)
+        {
+            Log.i(TAG, "|" + string + "|");
+        }
+        return result;
+    }
+
+    private void showExif(ExifInterface exif) {
+
+        String myAttribute = "[Exif information] \n\n";
+
+        myAttribute += getTagString(ExifInterface.TAG_DATETIME, exif); // 년:월:일 시:분:초
+        myAttribute += getTagString(ExifInterface.TAG_FLASH, exif);
+        myAttribute += getTagString(ExifInterface.TAG_GPS_AREA_INFORMATION, exif);
+        myAttribute += getTagString(ExifInterface.TAG_GPS_LATITUDE, exif); // 위도/경도
+        myAttribute += getTagString(ExifInterface.TAG_GPS_LATITUDE_REF, exif);
+        myAttribute += getTagString(ExifInterface.TAG_GPS_LONGITUDE, exif);
+        myAttribute += getTagString(ExifInterface.TAG_GPS_LONGITUDE_REF, exif);
+        myAttribute += getTagString(ExifInterface.TAG_MAKE, exif); // 휴대폰 제조사
+        myAttribute += getTagString(ExifInterface.TAG_MAKER_NOTE, exif);
+        myAttribute += getTagString(ExifInterface.TAG_MODEL, exif); // 모델명
+        myAttribute += getTagString(ExifInterface.TAG_ORIENTATION, exif);
+        myAttribute += getTagString(ExifInterface.TAG_WHITE_BALANCE, exif);
+        myAttribute += getTagString(ExifInterface.TAG_IMAGE_DESCRIPTION, exif);
+        myAttribute += getTagString(ExifInterface.TAG_IMAGE_UNIQUE_ID, exif); // 번호
+        myAttribute += getTagString(ExifInterface.TAG_IMAGE_LENGTH, exif);
+        myAttribute += getTagString(ExifInterface.TAG_IMAGE_WIDTH, exif);
+
+        Log.d(TAG, myAttribute) ;
+    }
+
+    @NonNull
+    static String getMimeType(@NonNull File file) {
+        String type = null;
+        final String url = file.toString();
+        final String extension = MimeTypeMap.getFileExtensionFromUrl(url);
+        if (extension != null) {
+            type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension.toLowerCase());
+        }
+        if (type == null) {
+            type = "image/*"; // fallback type. You might set it to */*
+        }
+        return type;
+    }
+
+    private String getTagString(String tag, ExifInterface exif) {
+        return (tag + " : " + exif.getAttribute(tag) + "\n");
+    }
+
+    /**
+     * wifi 사용중인지 확인
+     * @return
+     */
+    public boolean getWIFIStatus() {
+        ConnectivityManager cm = (ConnectivityManager) getBaseContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        boolean isWiFi = false ;
+        if (isConnected) {
+            isWiFi = activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
+        }
+        return isWiFi ;
+    }
 
     /**
      * 알람 매니져에 서비스 등록
      */
     private void registerRestartAlarm(){
 
-        Log.i("000 PersistentService" , "registerRestartAlarm" );
+        Log.d(TAG, "registerRestartAlarm" );
         Intent intent = new Intent(MediaPostService.this,RestartService.class);
         intent.setAction("ACTION.RESTART.PersistentService");
         PendingIntent sender = PendingIntent.getBroadcast(MediaPostService.this,0,intent,0);
@@ -128,7 +293,7 @@ public class MediaPostService extends Service {
      */
     private void unregisterRestartAlarm() {
 
-        Log.i("000 PersistentService", "unregisterRestartAlarm");
+        Log.i(TAG, "unregisterRestartAlarm");
 
         Intent intent = new Intent(MediaPostService.this, RestartService.class);
         intent.setAction("ACTION.RESTART.PersistentService");
